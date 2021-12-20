93b618801216a566cb36e8039c8baeca36b2ad8c
APHOMER <AAPHOMER@GMAIL.COM>
2021-12-20 22:59:54 +0100

    deleted first Line

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message1')
@@ -18,6 +16,5 @@ weatherForm.addEventListener('submit', (e) => {
 
 messageOne.textContent = 'Loading...'
 messageTwo.textContent = ''
-
 
 
