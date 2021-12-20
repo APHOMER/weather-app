579c8bc01e7c7e1963b56b14c6ea83b631cd0a87
APHOMER <AAPHOMER@GMAIL.COM>
2021-12-20 22:49:51 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+console.log('Client side javascript file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message1')
+const messageTwo = document.querySelector('#message2')
+
+messageOne.textContent = 'From javascript'
+messageTwo.textContent = 'From Html'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    console.log(location)
+})
+
+messageOne.textContent = 'Loading...'
+messageTwo.textContent = ''
+
+
+
